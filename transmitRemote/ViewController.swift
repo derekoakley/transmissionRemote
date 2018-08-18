e9476ec80f774abac9a9010cfa52c0499d96c574
Derek Oakley <derek@oakley.global>
2018-08-18 12:27:52 +0100

    Initial Commit

---
+++ transmitRemote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  transmitRemote
+//
+//  Created by Derek Oakley on 18/08/2018.
+//  Copyright © 2018 Derek Oakley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
